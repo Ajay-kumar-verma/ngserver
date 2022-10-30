ba3dfec6cd4cb6d5fd5d0242de2af7ff471cdf63
Siddhantsachin <91889081+Siddhantsachin@users.noreply.github.com>
2022-10-30 13:18:40 +0530

    server

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express=require('express');
 const router=express.Router();
 
+router.get("/",(req,res) =>{
+    res.send("Server is running !");
+    // console.log("Server is running ")
+})
+
 router.use('/signup',require('./signup'))
 router.use('/login',require('./login'))
 
